2931bb1105b482c889482c72c62fcc63ec42f66c
tang <tangj9604@gmail.com>
2026-02-01 15:52:58 +0800

    fix(MultipartTempFile) 自己实现临时文件 drop就删除

--- weblib/src/extract.rs
+++ weblib/src/extract.rs
@@ -10,21 +10,20 @@ use tokio::io::{AsyncWriteExt, BufWriter};
 struct Inner(Box<Path>);
 
 #[derive(Clone)]
-pub struct MultipartTempFile(Arc<Inner>);
+pub struct TempFile(Arc<Inner>);
 impl Drop for Inner {
     fn drop(&mut self) {
         let path = self.0.clone();
-        println!("dropping temp file {:?}", &path);
         tokio::task::spawn(async move { tokio::fs::remove_file(path).await });
     }
 }
-impl Deref for MultipartTempFile {
+impl Deref for TempFile {
     type Target = Path;
     fn deref(&self) -> &Self::Target {
-        &*self.0.0
+        &self.0.0
     }
 }
-impl MultipartTempFile {
+impl TempFile {
     pub fn new() -> Self {
         Self::with_suffix(".tmp")
     }
@@ -40,22 +39,22 @@ impl MultipartTempFile {
         File::create(self.deref()).await
     }
 }
-impl Default for MultipartTempFile {
+impl Default for TempFile {
     fn default() -> Self {
         Self::new()
     }
 }
 
 pub trait AsTempFile {
-    fn as_temp_file(&mut self) -> impl Future<Output = Result<MultipartTempFile, Error>>;
+    fn as_temp_file(&mut self) -> impl Future<Output = Result<TempFile, Error>>;
 }
 
 impl AsTempFile for Field<'_> {
-    async fn as_temp_file(&mut self) -> Result<MultipartTempFile, Error> {
+    async fn as_temp_file(&mut self) -> Result<TempFile, Error> {
         let file_name = self
             .file_name()
             .ok_or_else(|| Error::new(ErrorKind::InvalidFilename, "缺少文件名！"))?;
-        let multipart_temp_file = MultipartTempFile::with_suffix(file_name);
+        let multipart_temp_file = TempFile::with_suffix(file_name);
         let mut temp_file = multipart_temp_file.open().await?;
         async move {
             let mut buf_writer = BufWriter::new(&mut temp_file);
